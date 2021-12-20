fa7ec093f7ad2d687bc31a252ad481d3ff476f67
= <sai.c.coumar1@gmail.com>
2021-12-20 15:05:07 -0500

    user's queried

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], fetchAllMembers:true });
 
 
 client.on("reader", () => {
     console.log(`logged in as ${client.user.tag}!`)
+    let guild = client.guilds.cache.get("Server ID");
+    guild.members.cache.filter(member => !member.user.bot).forEach(member => console.log("===>>>", member.user.username));
 });
 
 client.on("message", msg => {
     if (msg.content === "tag"){
+        let server = msg.guild.id;
+        let channel = msg.channel.id;
         msg.reply("tagged");
+        const list = client.guilds.cache.get(server).members.cache; 
+        // console.log(list.length);
+        list.forEach(member => console.log(member.user.username)); 
+        
+
     }
 });
 
 
-client.login(process.env.TOKEN);
+client.login('OTIyNDk5OTk1MDcxMDUzODM0.YcCXEg.-hg3xed7OccvQnvBXf9BdkBK7WY');
